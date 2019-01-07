dde3877b991e5fdedfc1f83f7d72bce6c3a7d602
imranquadri3107 <imranquadri77@gmail.com>
2019-01-07 18:30:01 -0500

    Create Promises.js

---
+++ Promises.js
@@ -0,0 +1,23 @@
+// Promises in JavaScript. Why use promises
+// When you want to make an async call which has to wait for something to happen and then once it comes back you exit with a callback function. 
+//And withing that callback's function you might do another ajax call which can wait for another result and you would have another callback function.
+
+
+let promiseToClean = new Promise(function(resolve, reject){
+  // cleaning the room
+   let isClean = true;
+
+  if(isClean){
+    resolve('Clean');
+  } else{
+    reject('not Clean');
+  }
+
+
+});
+
+promiseToClean.then(function(fromResolve){
+  console.log('the room is '+ fromResolve);
+}).catch(function(fromReject){
+  console.log('Room is '+ fromReject);
+})
