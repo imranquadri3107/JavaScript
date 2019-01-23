6c6b3b25c70f1a7757b28be60de4a91b855168eb
imranquadri3107 <imranquadri77@gmail.com>
2019-01-22 19:36:35 -0800

    Create CheckPrime.js

---
+++ CheckPrime.js
@@ -0,0 +1,16 @@
+// Check Prime
+
+function isPrime(n){
+  var divisor = 2;
+
+  while(n > divisor){
+    if(n % divisor == 0){
+      return false
+    } else{
+      divisor++
+    }
+    return true;
+  }
+}
+
+isPrime(12);
