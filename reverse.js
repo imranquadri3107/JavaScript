0e1119897278d8b5f67ed9bdefa4716fcf740564
imranquadri3107 <imranquadri77@gmail.com>
2019-01-22 20:02:51 -0800

    Create reverse.js

---
+++ reverse.js
@@ -0,0 +1,50 @@
+// Reverse String
+type-1
+function reverse(str){
+  var rtnstr = '';
+
+  for(var i = str.length-1; i>=0; i--){
+
+    rtnstr+= str[i];
+  }
+  return rtnstr;
+}
+reverse(['hi' +' '+ 'hello' +' '+  'imran']);
+
+type-2
+
+function reverse(str){
+  var rtnstr = [];
+  
+  if(!str || typeof str !='string' || str.length <2)
+
+  return str;
+
+  for(var i=str.length-1; i>=0; i--){
+    rtnstr.push(str[i]);
+  }
+
+  return rtnstr.join('');
+}
+
+reverse('imranquadri')
+
+// type 3
+
+function reverse(str){
+  return str.split(' ').reverse()
+}
+
+reverse('imran is a good boy')
+
+
+
+
+
+
+
+
+
+
+
+
