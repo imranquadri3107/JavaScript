293a1b665f50917abfbf58a7a0356d471485eefd
imranquadri3107 <imranquadri77@gmail.com>
2019-01-30 23:44:25 -0800

    Update Stock_MaxProfit.js

--- Coding_Practice/Stock_MaxProfit.js
+++ Coding_Practice/Stock_MaxProfit.js
@@ -37,3 +37,42 @@ function maxProfit(arr){
 }
 
 maxProfit([44,30,24,32,35,30,40,38,15]);
+
+
+
+// Solution 2
+
+function maxPRofit(prices){
+  var min = Number.MAX_SAFE_INTEGER;
+  var max = 0;
+
+  for(var i = 0; i<prices.length-1; i++){
+    
+    if(prices[i] < min){
+      min = prices[i]
+      console.log(min)
+    }else if (prices[i]-min > max){
+      max = prices[i]-min
+      console.log(min)
+    }
+  } return max
+};
+
+var prices = [7, 1, 5, 6, 3];
+console.log('profit', maxPRofit(prices));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
