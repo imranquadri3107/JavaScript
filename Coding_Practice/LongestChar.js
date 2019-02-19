50376240b084e68a27d35cfcf5cb2431aff89d33
imranquadri3107 <imranquadri77@gmail.com>
2019-02-18 19:18:13 -0800

    Update LongestChar.js

--- Coding_Practice/LongestChar.js
+++ Coding_Practice/LongestChar.js
@@ -13,3 +13,23 @@ function findLongestWordLength(str) {
 }
 
 findLongestWordLength("The quick brown fox jumped over the lazy dog" );
+
+
+
+//Tab 2 for finding character more than length 6
+
+function findLongestWordLength(str) {
+  var newStr = str.split(' ')
+  var result = []
+  for(var i =0; i<newStr.length; i++){
+    var c = newStr[i];
+    if(c.length >= 6)
+     result.push(c)
+    else 
+    continue;
+  }
+  
+  return console.log(result, result.length);
+}
+
+findLongestWordLength("The quwsdeick brown foxertret jumped over the lazy dog" );
