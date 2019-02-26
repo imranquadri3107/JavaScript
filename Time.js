f31f46dca9bde4a565783f529e37863e97063a57
imranquadri3107 <imranquadri77@gmail.com>
2019-02-26 15:14:54 -0800

    Add files via upload

---
+++ Time.js
@@ -0,0 +1,21 @@
+
+// Function to write T in seconds, converts it into a string in the format <<hours>>h<<minutes>>m<<seconds>>s".
+
+function solution(T){
+  let totalSeconds = T;
+  let hours = Math.floor(totalSeconds/3600);
+
+  // console.log(hours);
+  
+  totalSeconds%=3600;
+  // console.log(totalSeconds);
+  
+  let minutes = Math.floor(totalSeconds / 60);
+
+  let seconds = totalSeconds % 60;
+
+  let S = hours + 'h' + minutes + 'm' + seconds + 's';
+  return(S);
+}
+
+solution(4600);
