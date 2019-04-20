101a947cd7e7c67bf4584cebe6ae5315ec981fc1
imranquadri3107 <imranquadri77@gmail.com>
2019-04-20 15:42:32 -0700

    Update Stack.js

--- DataStructure &Algorithms/Stack.js
+++ DataStructure &Algorithms/Stack.js
@@ -24,3 +24,26 @@ if (rword === word) {
 else {
    console.log(word + " is not a palindrome.");
 }
+
+'//////////////////////////////////////////////
+
+function isPalindrome(str){
+  var i, len = str.length;
+  for(i =0; i<len/2; i++){
+    if (str[i]!== str[len -1 -i])
+       return false;
+  }
+  return true;
+}
+
+> isPalindrome('madam')
+  = true
+> isPalindrome('toyota')
+  = false
+
+
+// Tab2
+
+function checkPalindrom(str) {
+    return str == str.split('').reverse().join('');
+}
